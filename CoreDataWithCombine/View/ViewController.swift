302566600ac603ecc12c091cc222863563a70003
joricky91 <jonathanricky91@gmail.com>
2023-02-11 12:58:00 +0700

    feat: create add new task functionality

--- CoreDataWithCombine/View/ViewController.swift
+++ CoreDataWithCombine/View/ViewController.swift
@@ -16,10 +16,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Todo List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(tapAddButton))
         view.backgroundColor = .systemBackground
         view.addSubview(tableView)
         setupConstraints()
         viewModel.fetchTodoItem()
+        setupBindings()
     }
 
     lazy var tableView: UITableView = {
@@ -50,6 +54,26 @@ class ViewController: UIViewController {
             tableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
             tableView.heightAnchor.constraint(equalTo: safeArea.heightAnchor),
         ])
+    }
+    
+    @objc func tapAddButton() {
+        let alert = UIAlertController(title: "Add Task", message: "Enter your new task", preferredStyle: .alert)
+        
+        alert.addTextField { field in
+            field.placeholder = "Enter task..."
+        }
+        alert.addAction(UIAlertAction(title: "Cancel", style: .destructive, handler: nil))
+        alert.addAction(UIAlertAction(title: "Add", style: .default, handler: { _ in
+            if let field = alert.textFields?.first {
+                if let text = field.text, !text.isEmpty {
+                    self.viewModel.addNewData(task: text)
+                    self.viewModel.fetchTodoItem()
+                }
+            }
+        }))
+        
+        present(alert, animated: true)
+        
     }
 
 }
