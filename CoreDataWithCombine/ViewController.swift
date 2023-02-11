d8dba873ad822a1292a011070d2f2d72537091d2
joricky91 <jonathanricky91@gmail.com>
2023-02-11 12:22:59 +0700

    Initial Commit

---
+++ CoreDataWithCombine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataWithCombine
+//
+//  Created by Jonathan Ricky Sandjaja on 11/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemTeal
+    }
+
+
+}
+
