3c8fefca43f7df3489b02477215e641ccd6b819d
joricky91 <jonathanricky91@gmail.com>
2023-02-11 12:34:50 +0700

    feat: creating table view

--- CoreDataWithCombine/View/ViewController.swift
+++ CoreDataWithCombine/View/ViewController.swift
@@ -12,9 +12,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemTeal
+        view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+        setupConstraints()
     }
 
+    lazy var tableView: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.delegate = self
+        table.dataSource = self
+        return table
+    }()
+    
+    func setupConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            tableView.heightAnchor.constraint(equalTo: safeArea.heightAnchor),
+        ])
+    }
 
 }
 
---
+++ CoreDataWithCombine/View/ViewControllerExtension.swift
@@ -0,0 +1,24 @@
+//
+//  ViewControllerExtension.swift
+//  CoreDataWithCombine
+//
+//  Created by Jonathan Ricky Sandjaja on 11/02/23.
+//
+
+import Foundation
+import UIKit
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        return cell
+    }
+    
+    
+}
